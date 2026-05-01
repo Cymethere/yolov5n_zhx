5bb6e06b83f5595946510b835aa82c46d474f675
UltralyticsAssistant <web@ultralytics.com>
2026-05-01 09:00:56 +0000

    Auto-format by https://ultralytics.com/actions

--- tensorboard_test.py
+++ tensorboard_test.py
@@ -1,29 +1,18 @@
 import torch
 import torchvision
-from torchvision import datasets, transforms
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()   # 会在当前目录创建 runs/ 文件夹存放日志
+from torchvision import datasets, transforms
+
+writer = SummaryWriter()  # 会在当前目录创建 runs/ 文件夹存放日志
 
 # Compose a set of transforms to use later on
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 # Load in the MNIST dataset
-trainset = datasets.MNIST(
-    'mnist_train',
-    train=True,
-    download=True,
-    transform=transform
-)
+trainset = datasets.MNIST("mnist_train", train=True, download=True, transform=transform)
 
 # Create a data loader
-trainloader = torch.utils.data.DataLoader(
-    trainset,
-    batch_size=64,
-    shuffle=True
-)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 # Get a pre-trained ResNet18 model
 model = torchvision.models.resnet18(False)
@@ -36,6 +25,6 @@ images, labels = next(iter(trainloader))
 
 # Write the data to TensorBoard
 grid = torchvision.utils.make_grid(images)
-writer.add_image('images', grid, 0)
+writer.add_image("images", grid, 0)
 writer.add_graph(model, images)
 writer.close()
